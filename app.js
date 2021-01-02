5e6519a25067e5146fdf9f16af6adc6302927ad0
unknown <aj.vasanth555@gmail.com>
2021-01-02 19:38:24 +0530

    second

--- app.js
+++ app.js
@@ -56,8 +56,7 @@ app.post("/",function(req,res)
 app.post("/failure",function(req,res){
     res.redirect("/")
 })
-app.listen(3000,function()
-{
+app.listen(process.env.PORT || 3000,function(){
     console.log("RUNNING");
 });
 
